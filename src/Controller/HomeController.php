531a73fc3fa829ec39cc5a321ab938fc1585364a
Borisdmk <151534419+Borisdmk@users.noreply.github.com>
2024-05-13 16:36:58 +0200

    Back office, products et category bien a jour et lie

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,39 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Category;
+use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Translation\TranslatableMessage;
 
 class HomeController extends AbstractController
 {
+    // symfony dit : si tu as /home tu appelles cette fonction, et tu affiches ceci
+    // controller_name = homeController (variables de vues)
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository, PaginatorInterface $paginator, Request $request): Response
     {
+
+        // $msg_acceuil = new TranslatableMessage('its the message to translate');
+        $products = $paginator->paginate(
+            $productRepository->findAll(), /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            2 /*limit per page*/
+        );
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'fabrice' => 'Hey voici ma variable de vue Fabrice',
+            // 'msg_acceuil' => $msg_acceuil,
+            'categories' => $categoryRepository->findAll(),
+            'products' => $products,
         ]);
     }
 }
