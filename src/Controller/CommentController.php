3983eb77115e3622be2fb5a5c8bddf923ca6b698
Borisdmk <151534419+Borisdmk@users.noreply.github.com>
2024-06-17 13:50:40 +0200

    affichage commentaire et suppression, changement de style et colonne pour la page boutique

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -7,11 +7,12 @@ namespace App\Controller;
 use App\Entity\Comment;
 use App\Form\CommentType;
 use App\Repository\CommentRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface; // Utilisation de l'EntityManager pour les opérations de persistance
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; // Utilisation du contrôleur de base de Symfony
+use Symfony\Component\HttpFoundation\Request; // Utilisation de l'objet Request pour gérer les requêtes HTTP
+use Symfony\Component\HttpFoundation\Response; // Utilisation de l'objet Response pour envoyer des réponses HTTP
+use Symfony\Component\Routing\Annotation\Route; // Utilisation des annotations de routage
+use Symfony\Component\Security\Core\Exception\AccessDeniedException; // Utilisation de l'exception AccessDeniedException pour gérer les accès non autorisés
 
 #[Route('/comment')]
 class CommentController extends AbstractController
@@ -19,39 +20,53 @@ class CommentController extends AbstractController
     #[Route('/new', name: 'comment_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $comment = new Comment();
-        $form = $this->createForm(CommentType::class, $comment);
-        $form->handleRequest($request);
+        $comment = new Comment(); // Création d'une nouvelle instance de Comment
+        $form = $this->createForm(CommentType::class, $comment); // Création du formulaire pour l'entité Comment
+        $form->handleRequest($request); // Gestion de la requête par le formulaire
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $comment->setDate(new \DateTime());
+        if ($form->isSubmitted() && $form->isValid()) { // Vérification si le formulaire a été soumis et est valide
+            $comment->setDate(new \DateTime()); // Définition de la date du commentaire à la date actuelle
             $comment->setUser($this->getUser()); // Associer le commentaire à l'utilisateur connecté
-            $entityManager->persist($comment);
-            $entityManager->flush();
+            $entityManager->persist($comment); // Persistance du commentaire
+            $entityManager->flush(); // Envoi des modifications à la base de données
 
-            return $this->redirectToRoute('app_profile_show', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_profile_show', [], Response::HTTP_SEE_OTHER); // Redirection vers la route 'app_profile_show'
         }
 
         return $this->render('comment/new.html.twig', [
-            'comment' => $comment,
-            'form' => $form,
+            'comment' => $comment, // Passage de l'entité Comment à la vue
+            'form' => $form, // Passage du formulaire à la vue
         ]);
     }
 
     #[Route('/', name: 'comment_index', methods: ['GET'])]
     public function index(CommentRepository $commentRepository): Response
     {
-        return $this->render('comment/index.html.twig', [
-            'comments' => $commentRepository->findAll(),
+        return $this->render('comment/index.html.twig', [ // Rendu de la vue pour afficher tous les commentaires
+            'comments' => $commentRepository->findAll(), // Passage de tous les commentaires à la vue
         ]);
     }
 
     #[Route('/user', name: 'comment_user', methods: ['GET'])]
     public function userComments(CommentRepository $commentRepository): Response
     {
-        $user = $this->getUser();
-        return $this->render('comment/user.html.twig', [
-            'comments' => $commentRepository->findBy(['user' => $user]),
+        $user = $this->getUser(); // Récupération de l'utilisateur connecté
+        return $this->render('comment/user.html.twig', [ // Rendu de la vue pour afficher les commentaires de l'utilisateur
+            'comments' => $commentRepository->findBy(['user' => $user]), // Passage des commentaires de l'utilisateur à la vue
         ]);
+    }
+
+    #[Route('/delete/{id}', name: 'comment_delete', methods: ['POST'])]
+    public function delete(Request $request, Comment $comment, EntityManagerInterface $entityManager): Response
+    {
+        if ($comment->getUser() !== $this->getUser()) { // Vérification si l'utilisateur connecté est bien l'auteur du commentaire
+            throw new AccessDeniedException('Vous n\'avez pas le droit de supprimer ce commentaire.');
+        }
+
+        $entityManager->remove($comment); // Suppression du commentaire
+        $entityManager->flush();  // Envoi des modifications à la base de données
+
+ 
+        return $this->redirectToRoute('app_profile'); // Redirection vers la route 'app_profile'
     }
 }
